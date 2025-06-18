ece862f14b10a39395055d74f105f74bda08c97d
A M Lohit <amlohit@As-MacBook-Air.local>
2025-06-18 23:45:52 +0530

    Swagger documentation added.

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import blogRoutes from './routes/blog';
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load( __dirname + '/docs/openapi.yaml');
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1', [authRoutes, blogRoutes]);
 
